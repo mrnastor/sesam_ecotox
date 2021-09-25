417a01945255f1289e52d6c5341215e28ff774d5
Benson Nastor <mrnastor@gmail.com>
2021-09-25 21:32:04 +0800

    Upgrade to gatsby v3

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,11 @@ const { createFilePath } = require(`gatsby-source-filesystem`);
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
-      modules: [path.resolve(__dirname, `src`), `node_modules`]
+      modules: [path.resolve(__dirname, `src`), `node_modules`],
+      fallback: {
+        path: require.resolve("path-browserify"),
+        util: require.resolve("util/")
+      }
     }
   });
 };
